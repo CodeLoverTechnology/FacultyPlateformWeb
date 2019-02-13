1ca6566418274f30471e34221ee46e0d9e091cc8
RAHUL KUMAR <rahulinbscit@live.com>
2019-02-13 05:47:50 +0530

    Add project files.

---
+++ AllFacultyInSinglePlatformWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllFacultyInSinglePlatformWeb.Startup))]
+namespace AllFacultyInSinglePlatformWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
